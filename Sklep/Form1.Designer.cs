cd8682f18c53611e9089c5203dc5466d3d7cb701
Domikelele <128388455+Domikelele@users.noreply.github.com>
2025-02-09 12:29:52 +0100

    Dodaj pliki projektów.

---
+++ Sklep/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            listOfUsers = new ListBox();
+            markaTextBox = new TextBox();
+            nazwaTextBox = new TextBox();
+            cenaTextBox = new TextBox();
+            iloscTextBox = new TextBox();
+            addButton = new Button();
+            searchIdLabel = new Label();
+            searchIdTextBox = new TextBox();
+            searchIdButton = new Button();
+            markaLabel = new Label();
+            nazwaLabel = new Label();
+            cenaLabel = new Label();
+            iloscLabel = new Label();
+            searchMarkaLabel = new Label();
+            searchMarkaTextBox = new TextBox();
+            searchMarkaButton = new Button();
+            showAllButton = new Button();
+            idToDeleteLabel = new Label();
+            idToDeleteTextBox = new TextBox();
+            deleteButton = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(56, 72);
+            label1.Name = "label1";
+            label1.Size = new Size(68, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Lista butów";
+            // 
+            // listOfUsers
+            // 
+            listOfUsers.FormattingEnabled = true;
+            listOfUsers.ItemHeight = 15;
+            listOfUsers.Location = new Point(43, 144);
+            listOfUsers.Name = "listOfUsers";
+            listOfUsers.Size = new Size(247, 259);
+            listOfUsers.TabIndex = 2;
+            // 
+            // markaTextBox
+            // 
+            markaTextBox.Location = new Point(350, 100);
+            markaTextBox.Name = "markaTextBox";
+            markaTextBox.Size = new Size(150, 23);
+            markaTextBox.TabIndex = 4;
+            // 
+            // nazwaTextBox
+            // 
+            nazwaTextBox.Location = new Point(350, 150);
+            nazwaTextBox.Name = "nazwaTextBox";
+            nazwaTextBox.Size = new Size(150, 23);
+            nazwaTextBox.TabIndex = 6;
+            // 
+            // cenaTextBox
+            // 
+            cenaTextBox.Location = new Point(350, 200);
+            cenaTextBox.Name = "cenaTextBox";
+            cenaTextBox.Size = new Size(150, 23);
+            cenaTextBox.TabIndex = 8;
+            // 
+            // iloscTextBox
+            // 
+            iloscTextBox.Location = new Point(350, 250);
+            iloscTextBox.Name = "iloscTextBox";
+            iloscTextBox.Size = new Size(150, 23);
+            iloscTextBox.TabIndex = 10;
+            // 
+            // addButton
+            // 
+            addButton.Location = new Point(350, 280);
+            addButton.Name = "addButton";
+            addButton.Size = new Size(150, 30);
+            addButton.TabIndex = 11;
+            addButton.Text = "Dodaj Produkt";
+            addButton.UseVisualStyleBackColor = true;
+            addButton.Click += addButton_Click;
+            // 
+            // searchIdLabel
+            // 
+            searchIdLabel.AutoSize = true;
+            searchIdLabel.Location = new Point(550, 72);
+            searchIdLabel.Name = "searchIdLabel";
+            searchIdLabel.Size = new Size(21, 15);
+            searchIdLabel.TabIndex = 12;
+            searchIdLabel.Text = "ID:";
+            // 
+            // searchIdTextBox
+            // 
+            searchIdTextBox.Location = new Point(550, 100);
+            searchIdTextBox.Name = "searchIdTextBox";
+            searchIdTextBox.Size = new Size(150, 23);
+            searchIdTextBox.TabIndex = 13;
+            // 
+            // searchIdButton
+            // 
+            searchIdButton.Location = new Point(550, 130);
+            searchIdButton.Name = "searchIdButton";
+            searchIdButton.Size = new Size(150, 30);
+            searchIdButton.TabIndex = 14;
+            searchIdButton.Text = "Szukaj po ID";
+            searchIdButton.UseVisualStyleBackColor = true;
+            searchIdButton.Click += searchIdButton_Click;
+            // 
+            // markaLabel
+            // 
+            markaLabel.AutoSize = true;
+            markaLabel.Location = new Point(350, 72);
+            markaLabel.Name = "markaLabel";
+            markaLabel.Size = new Size(40, 15);
+            markaLabel.TabIndex = 3;
+            markaLabel.Text = "Marka";
+            // 
+            // nazwaLabel
+            // 
+            nazwaLabel.AutoSize = true;
+            nazwaLabel.Location = new Point(350, 130);
+            nazwaLabel.Name = "nazwaLabel";
+            nazwaLabel.Size = new Size(42, 15);
+            nazwaLabel.TabIndex = 5;
+            nazwaLabel.Text = "Nazwa";
+            // 
+            // cenaLabel
+            // 
+            cenaLabel.AutoSize = true;
+            cenaLabel.Location = new Point(350, 180);
+            cenaLabel.Name = "cenaLabel";
+            cenaLabel.Size = new Size(34, 15);
+            cenaLabel.TabIndex = 7;
+            cenaLabel.Text = "Cena";
+            // 
+            // iloscLabel
+            // 
+            iloscLabel.AutoSize = true;
+            iloscLabel.Location = new Point(350, 230);
+            iloscLabel.Name = "iloscLabel";
+            iloscLabel.Size = new Size(61, 15);
+            iloscLabel.TabIndex = 9;
+            iloscLabel.Text = "Ilość sztuk";
+            // 
+            // searchMarkaLabel
+            // 
+            searchMarkaLabel.AutoSize = true;
+            searchMarkaLabel.Location = new Point(550, 230);
+            searchMarkaLabel.Name = "searchMarkaLabel";
+            searchMarkaLabel.Size = new Size(43, 15);
+            searchMarkaLabel.TabIndex = 15;
+            searchMarkaLabel.Text = "Marka:";
+            // 
+            // searchMarkaTextBox
+            // 
+            searchMarkaTextBox.Location = new Point(550, 250);
+            searchMarkaTextBox.Name = "searchMarkaTextBox";
+            searchMarkaTextBox.Size = new Size(150, 23);
+            searchMarkaTextBox.TabIndex = 16;
+            // 
+            // searchMarkaButton
+            // 
+            searchMarkaButton.Location = new Point(550, 280);
+            searchMarkaButton.Name = "searchMarkaButton";
+            searchMarkaButton.Size = new Size(150, 30);
+            searchMarkaButton.TabIndex = 17;
+            searchMarkaButton.Text = "Szukaj po marce";
+            searchMarkaButton.UseVisualStyleBackColor = true;
+            searchMarkaButton.Click += searchMarkaButton_Click;
+            // 
+            // showAllButton
+            // 
+            showAllButton.Location = new Point(40, 100);
+            showAllButton.Name = "showAllButton";
+            showAllButton.Size = new Size(150, 30);
+            showAllButton.TabIndex = 18;
+            showAllButton.Text = "Pokaż Wszystko";
+            showAllButton.UseVisualStyleBackColor = true;
+            showAllButton.Click += showAllButton_Click;
+            // 
+            // idToDeleteLabel
+            // 
+            idToDeleteLabel.AutoSize = true;
+            idToDeleteLabel.Location = new Point(750, 72);
+            idToDeleteLabel.Name = "idToDeleteLabel";
+            idToDeleteLabel.Size = new Size(88, 15);
+            idToDeleteLabel.TabIndex = 19;
+            idToDeleteLabel.Text = "ID do usunięcia";
+            // 
+            // idToDeleteTextBox
+            // 
+            idToDeleteTextBox.Location = new Point(750, 100);
+            idToDeleteTextBox.Name = "idToDeleteTextBox";
+            idToDeleteTextBox.Size = new Size(150, 23);
+            idToDeleteTextBox.TabIndex = 20;
+            // 
+            // deleteButton
+            // 
+            deleteButton.Location = new Point(750, 130);
+            deleteButton.Name = "deleteButton";
+            deleteButton.Size = new Size(150, 30);
+            deleteButton.TabIndex = 21;
+            deleteButton.Text = "Usuń But";
+            deleteButton.UseVisualStyleBackColor = true;
+            deleteButton.Click += deleteButton_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(945, 450);
+            Controls.Add(addButton);
+            Controls.Add(iloscTextBox);
+            Controls.Add(cenaTextBox);
+            Controls.Add(nazwaTextBox);
+            Controls.Add(markaTextBox);
+            Controls.Add(listOfUsers);
+            Controls.Add(label1);
+            Controls.Add(markaLabel);
+            Controls.Add(nazwaLabel);
+            Controls.Add(cenaLabel);
+            Controls.Add(iloscLabel);
+            Controls.Add(searchIdButton);
+            Controls.Add(searchIdTextBox);
+            Controls.Add(searchIdLabel);
+            Controls.Add(searchMarkaButton);
+            Controls.Add(searchMarkaTextBox);
+            Controls.Add(searchMarkaLabel);
+            Controls.Add(showAllButton);
+            Controls.Add(deleteButton);
+            Controls.Add(idToDeleteTextBox);
+            Controls.Add(idToDeleteLabel);
+            Name = "Form1";
+            Text = "Sklep - Dodaj Buty";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private ListBox listOfUsers;
+        private TextBox markaTextBox;
+        private TextBox nazwaTextBox;
+        private TextBox cenaTextBox;
+        private TextBox iloscTextBox;
+        private Button addButton;
+        private Label markaLabel;
+        private Label nazwaLabel;
+        private Label cenaLabel;
+        private Label iloscLabel;
+        private Label searchIdLabel;
+        private TextBox searchIdTextBox;
+        private Button searchIdButton;
+        private Label searchMarkaLabel;
+        private TextBox searchMarkaTextBox;
+        private Button searchMarkaButton;
+        private Button showAllButton;
+        private Button deleteButton;
+        private TextBox idToDeleteTextBox;
+        private Label idToDeleteLabel;
+    }
+}
---
+++ Sklep/Form1.cs
@@ -0,0 +1,233 @@
+using System;
+using System.Windows.Forms;
+using Microsoft.Data.Sqlite;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private SqliteConnection dbConnection;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeDatabase();
+            LoadShoesFromDatabase();  // Za³adowanie danych o butach z bazy
+        }
+
+        // Inicjalizacja bazy danych
+        private void InitializeDatabase()
+        {
+            dbConnection = new SqliteConnection("Data Source=Sklep.db"); // U¿ywamy bazy danych Sklep.db
+            dbConnection.Open();
+
+            // Zmiana zapytania tworz¹cego tabelê buty
+            string createTableQuery = @"
+                CREATE TABLE IF NOT EXISTS buty (
+                    Id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                    Marka TEXT NOT NULL, 
+                    Nazwa TEXT NOT NULL, 
+                    Cena REAL NOT NULL, 
+                    IloscSztuk INTEGER NOT NULL
+                )";
+
+            using (var command = new SqliteCommand(createTableQuery, dbConnection))
+            {
+                command.ExecuteNonQuery();
+            }
+        }
+
+        // Wczytanie butów z bazy danych
+        private void LoadShoesFromDatabase()
+        {
+            listOfUsers.Items.Clear();
+
+            // Sprawdzenie, czy tabela istnieje
+            string checkTableQuery = "SELECT name FROM sqlite_master WHERE type='table' AND name='buty'";
+            using (var checkCommand = new SqliteCommand(checkTableQuery, dbConnection))
+            {
+                var result = checkCommand.ExecuteScalar();
+
+                if (result == null)
+                {
+                    // Tabela nie istnieje, wiêc j¹ tworzymy
+                    string createTableQuery = @"
+                        CREATE TABLE IF NOT EXISTS buty (
+                            Id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                            Marka TEXT NOT NULL, 
+                            Nazwa TEXT NOT NULL, 
+                            Cena REAL NOT NULL, 
+                            IloscSztuk INTEGER NOT NULL
+                        )";
+
+                    using (var createCommand = new SqliteCommand(createTableQuery, dbConnection))
+                    {
+                        createCommand.ExecuteNonQuery();
+                    }
+                    return; // Nie próbujemy wczytaæ danych, bo tabela jest pusta
+                }
+            }
+
+            // Jeœli tabela istnieje, wczytujemy dane
+            string selectQuery = "SELECT Id, Marka, Nazwa, Cena, IloscSztuk FROM buty";
+            using (var command = new SqliteCommand(selectQuery, dbConnection))
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    listOfUsers.Items.Add($"{reader.GetInt32(0)} - {reader.GetString(1)} {reader.GetString(2)} - {reader.GetDouble(3):C} - {reader.GetInt32(4)} szt.");
+                }
+            }
+        }
+
+        // Dodanie buta do bazy po klikniêciu przycisku
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            string marka = markaTextBox.Text.Trim();
+            string nazwa = nazwaTextBox.Text.Trim();
+            double cena;
+            int iloscSztuk;
+
+            // Sprawdzenie, czy dane s¹ poprawne
+            if (string.IsNullOrWhiteSpace(marka) || string.IsNullOrWhiteSpace(nazwa) ||
+                !double.TryParse(cenaTextBox.Text, out cena) || !int.TryParse(iloscTextBox.Text, out iloscSztuk))
+            {
+                MessageBox.Show("Proszê wprowadziæ poprawne dane.", "B³¹d", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            try
+            {
+                // Zapytanie do dodania produktu
+                string insertQuery = "INSERT INTO buty (Marka, Nazwa, Cena, IloscSztuk) VALUES (@Marka, @Nazwa, @Cena, @IloscSztuk)";
+                using (var command = new SqliteCommand(insertQuery, dbConnection))
+                {
+                    command.Parameters.AddWithValue("@Marka", marka);
+                    command.Parameters.AddWithValue("@Nazwa", nazwa);
+                    command.Parameters.AddWithValue("@Cena", cena);
+                    command.Parameters.AddWithValue("@IloscSztuk", iloscSztuk);
+                    command.ExecuteNonQuery();
+                }
+
+                LoadShoesFromDatabase();  // Odœwie¿enie listy butów po dodaniu
+                markaTextBox.Clear();
+                nazwaTextBox.Clear();
+                cenaTextBox.Clear();
+                iloscTextBox.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Wyst¹pi³ b³¹d: {ex.Message}", "B³¹d", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void searchIdButton_Click(object sender, EventArgs e)
+        {
+            string id = searchIdTextBox.Text.Trim();
+
+            if (!int.TryParse(id, out int shoeId))
+            {
+                MessageBox.Show("Proszê wprowadziæ poprawne ID.", "B³¹d", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            listOfUsers.Items.Clear();
+
+            string selectQuery = "SELECT Id, Marka, Nazwa, Cena, IloscSztuk FROM buty WHERE Id = @Id";
+            using (var command = new SqliteCommand(selectQuery, dbConnection))
+            {
+                command.Parameters.AddWithValue("@Id", shoeId);
+                using (var reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        listOfUsers.Items.Add($"{reader.GetInt32(0)} - {reader.GetString(1)} {reader.GetString(2)} - {reader.GetDouble(3):C} - {reader.GetInt32(4)} szt.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Nie znaleziono buta o takim ID.", "Brak wyników", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+        }
+
+        private void searchMarkaButton_Click(object sender, EventArgs e)
+        {
+            string marka = searchMarkaTextBox.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(marka))
+            {
+                MessageBox.Show("Proszê wprowadziæ nazwê marki.", "B³¹d", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            listOfUsers.Items.Clear();
+
+            string selectQuery = "SELECT Id, Marka, Nazwa, Cena, IloscSztuk FROM buty WHERE Marka LIKE @Marka";
+            using (var command = new SqliteCommand(selectQuery, dbConnection))
+            {
+                command.Parameters.AddWithValue("@Marka", "%" + marka + "%");
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        listOfUsers.Items.Add($"{reader.GetInt32(0)} - {reader.GetString(1)} {reader.GetString(2)} - {reader.GetDouble(3):C} - {reader.GetInt32(4)} szt.");
+                    }
+                }
+            }
+        }
+
+        private void showAllButton_Click(object sender, EventArgs e)
+        {
+            LoadShoesFromDatabase(); // Wywo³anie metody ³aduj¹cej wszystkie buty
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            string idText = idToDeleteTextBox.Text.Trim();
+
+            if (!int.TryParse(idText, out int shoeId))
+            {
+                MessageBox.Show("Proszê wprowadziæ poprawne ID.", "B³¹d", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            try
+            {
+                string deleteQuery = "DELETE FROM buty WHERE Id = @Id";
+                using (var command = new SqliteCommand(deleteQuery, dbConnection))
+                {
+                    command.Parameters.AddWithValue("@Id", shoeId);
+                    int rowsAffected = command.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                    {
+                        MessageBox.Show("But zosta³ usuniêty.", "Sukces", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Nie znaleziono buta o takim ID.", "B³¹d", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+
+                LoadShoesFromDatabase(); // Odœwie¿enie listy butów po usuniêciu
+                idToDeleteTextBox.Clear(); // Czyszczenie pola tekstowego
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Wyst¹pi³ b³¹d: {ex.Message}", "B³¹d", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        // Zamkniêcie po³¹czenia przy zamykaniu aplikacji
+        protected override void OnFormClosed(FormClosedEventArgs e)
+        {
+            dbConnection?.Close();
+            dbConnection?.Dispose();
+            base.OnFormClosed(e);
+        }
+
+        
+    }
+}
